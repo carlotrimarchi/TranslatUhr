25f242f4b53fbd652b30027c75ff2e9a30a06d79
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2025-02-21 21:18:12 +0100

    Modify tests to expect object as result; modify tests to work with 12 hour system

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -4,38 +4,43 @@ import { numbersMap } from "../constants.ts";
 import GermanTimeParser from "../GermanTimeParser.ts";
 
 describe("GermanTimeParser", () => {
-const cases = {
-"Absolute time (hour only)": [
-  ["drei uhr", "15:00"],
-  ["fünf uhr", "17:00"],
-],
-"Absolute time with minutes": [
-  ["drei uhr zwanzig", "15:20"],
-  ["vier uhr vierzig", "16:40"],
-  ["fünf uhr zwanzig", "17:20"],
-],
-"Relative time - 'halb'": [
-  ["halb drei", "14:30"],
-  ["halb vier", "15:30"],
-  ["halb fünf", "16:30"],
-  ["halb sechs", "17:30"],
-],
-"Relative time - 'nach'": [
-  ["viertel nach sechs", "18:15"],
-  ["zwanzig nach vier", "16:20"],
-],
-"Relative time - 'vor'": [
-  ["zwanzig vor vier", "15:40"],
-  ["viertel vor sechs", "17:45"],
-]
-  // ["dreiviertel sieben", "18:45"],
-  // ["dreiviertel nach sieben", "19:45"],
-};
+  const cases = {
+    "Absolute time (hour only)": [
+      ["drei uhr", { hour: 3, minutes: 0 }],
+      ["fünf uhr", { hour: 5, minutes: 0 }],
+    ],
+    "Absolute time with minutes": [
+      ["drei uhr zwanzig", { hour: 3, minutes: 20 }],
+      ["vier uhr vierzig", { hour: 4, minutes: 40 }],
+      ["fünf uhr zwanzig", { hour: 5, minutes: 20 }],
+    ],
+    "Relative time - 'halb'": [
+      ["halb drei", { hour: 2, minutes: 30 }],
+      ["halb vier", { hour: 3, minutes: 30 }],
+      ["halb fünf", { hour: 4, minutes: 30 }],
+      ["halb sechs", { hour: 5, minutes: 30 }],
+    ],
+    "Relative time - 'nach'": [
+      ["viertel nach sechs", { hour: 6, minutes: 15 }],
+      ["zwanzig nach vier", { hour: 4, minutes: 20 }],
+    ],
+    "Relative time - 'vor'": [
+      ["zwanzig vor vier", { hour: 3, minutes: 40 }],
+      ["viertel vor sechs", { hour: 5, minutes: 45 }],
+    ],
+    // ["dreiviertel sieben", "18:45"],
+    // ["dreiviertel nach sieben", "19:45"],
+  };
   for (const [category, tests] of Object.entries(cases)) {
     describe(category, () => {
-      test.each(tests)("should translate '%s' correctly:", (input, expected) => {
-        expect(new GermanTimeParser(input, numbersMap).parse()).toBe(expected);
-      });
+      test.each(tests)(
+        "should translate '%s' correctly:",
+        (input, expected) => {
+          expect(new GermanTimeParser(input, numbersMap).parse()).toEqual(
+            expected,
+          );
+        },
+      );
     });
   }
 });
